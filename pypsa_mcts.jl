22f8e328566159f8493b5824d2b99a7ea4e9a0e4
ktehranchi <83722342+ktehranchi@users.noreply.github.com>
2023-03-15 15:14:40 -0700

    transition reward function

--- pypsa_mcts.jl
+++ pypsa_mcts.jl
@@ -10,37 +10,111 @@ using PyCall
 so = pyimport("pypsa")
 
 using QuickPOMDPs: QuickPOMDP
-# using POMDPTools: ImplicitDistribution
+using Random, Distributions
+Random.seed!(123)
 
-pypsa_PCS = QuickPOMDP(
-    actions = ['build500kv', 'no_build', 'buildHVDC'],
-    discount = 0.95,
 
-    gen = function (s, a, rng)
-        random_cost_to_build = randn(rng)
 
+γ = 0.95
+S = # int, number of states year and what lines have been built  [2020 1 1]
+A = ["build500kv", "buildHVDC"] # vector of actions
+TR = # function sp, r = TR(s,a)
+# TR function calls on T and R function given (s,a) and yields next state and reward
+
+
+
+pypsa_MDP = QuickPOMDP(
+    γ = 0.95,
+    S_init = (2023,0,0) #Vector of interger state variables (year, HVAC, HVDC Undersea)
+    A = ['build500kv', 'buildHVDC'],
+
+    TR = function (state, action)
         py"""
         import pypsa
-        clustered_path='/Users/kamrantehranchi/Library/CloudStorage/OneDrive-Stanford/Kamran_OSW/PyPSA_Models/pypsa-breakthroughenergy-usa/workflow/resources/western/elec_s_50.nc'
-        network = pypsa.Network(clustered_path)
+        network_path='/Users/kamrantehranchi/Documents/GradSchool/Courses/Q5/Decision Making under Uncertainty/DMuU_TransmissionExpansion/pypsa_60_multiyear.nc'
+        network = pypsa.Network(network_path)
 
-        def WECC_simulator(random_cost_to_build,a):
-            #use action and random_cost_to_build to modify the network by specified action and cost
+        ows_cf = pd.read_csv('/Users/kamrantehranchi/Documents/GradSchool/Courses/Q5/Decision Making under Uncertainty/DMuU_TransmissionExpansion/OSW_CF_Humboldt.csv')
 
-            network.lopf(network.snapshots[0:48], solver_name="gurobi")
-            return network.generators_t.p.sum().sum()
+        def add_hvdc(n,num):
+            for i in range(0,num):
+                n.add("Link", f'New_HVDC_{i}', bus0="CISO-PGAE0 13", bus1="CISO-PGAE0 8",
+                    type="HVDC_VSC", carrier = "DC", efficiency=1, p_nom=100,p_min_pu=-1,)
+            return n
+        
+        def add_hvac(n,num):
+            for i in range(0,num):
+                n.add("Line", f'New_HVAC_{i}', bus0="CISO-PGAE0 13", bus1="CISO-PGAE0 16",
+                    r=0.815803, x=6.873208, b=0.000398 ,s_nom=536.51,length=400, 
+                    type="Rail",v_nom=230, x_pu= 0.000725, r_pu= 0.000085, g_pu= 0.0,
+                    b_pu= 7.557957, x_pu_eff= 0.000725, r_pu_eff= 0.000085, s_nom_opt= 2100.295202,)
+            return n
+
+        def add_ows(n,num):
+            for i in range(0,num):
+                n.add("Generator", f'Humboldt_OSW_{i}', bus= "CISO-PGAE0 13", carrier="offwind",
+                    p_nom=4000, marginal_cost=0, p_max_pu=np.tile(ows_cf.cf_humboldt.values,6),)
+            return n
+
+        def update_network(n, state, action):
+            year = state[0]
+            num_hvac = state[1]
+            num_hvdc = state[2]
+
+            if action == "build500kv":
+                num_hvac += 1
+            elif action == "buildHVDC":
+                num_hvdc += 1
+            
+            add_hvac(n, num_hvac)
+            add_hvdc(n, num_hvdc)
+            add_ows(n, 1)
+            
+            new_state = (year, num_hvac, num_hvdc)
+            return n , new_state
+
+        def WECC_simulator(state, action):
+            network, state= update_network(network, state, action)
+            sim_year = state[0]
+            simulation_hours = 168
+
+            sim_snapshot = network.snapshots[network.snapshots.get_level_values(0) == sim_year][:simulation_hours]
+            network.lopf(sim_snapshot, solver_name="gurobi")
+
+            generation_ts = network.generators_t.p.loc[sim_snapshot]
+            operation_costs = network.generators.marginal_cost * generation_ts
+            print("Total Operational Cost:  " , operation_costs.sum(axis=1).sum())
+            return operating_cost, state
+        
         """
 
-        py"WECC_simulator"(s,a,random_cost_to_build)
+        annualize = function (cost,capital_γ,lifetime)
+            return (cost * capital_γ) / (1 - (1 + capital_γ)^-lifetime)
+        end
 
-        annualsed_cost = our own calculation based on the action and random_cost_to_build   
+        capital_discount_rate = 0.05
+        lifetime = 20
+        hvac_cost = [300e6, 400e6]
+        hvdc_cost = [400e6, 500e6]
+
+        hvac_cost_dist = Uniform(hvac_cost[1],hvac_cost[2])
+        hvac_cost_dist = Uniform(hvdc_cost[1],hvdc_cost[2])
         
-        operating_cost= py"WECC_simulator"(s,a,random_cost_to_build)
-        return (sp=(xp, vp), r=r)
-    end,
+        if action = 'build500kv':
+            random_cost_to_build = rand(hvac_cost_dist, 1)
+        else:
+            random_cost_to_build = rand(hvdc_cost_dist, 1)
 
-    initialstate = [0,0,0]
-)
+        annualized_cost = annualize(random_cost_to_build) 
+
+        operating_cost, new_state = py"WECC_simulator"(state,action)
+
+        annual_reward = operating_cost + annualized_cost
+
+        return (sp=new_state, r=annual_reward)
+
+
+    end,
 
 
 
